80192680f40085d748dec24a80777f886bfa738a
Nguyễn Đức Anh <99268985+GLNihilis@users.noreply.github.com>
2023-03-02 02:47:33 +0700

    Update_DB

--- configs/DBConnection.php
+++ configs/DBConnection.php
@@ -1,12 +1,11 @@
 <?php
-
 class DBConnection{
     private $conn=null;
 
     public function __construct(){
          // B1. Kết nối DB Server
          try {
-            $this->conn = new PDO('mysql:host=localhost;dbname=demo_;port=3306', 'root','');
+            $this->conn = new PDO('mysql:host=localhost;dbname=btth02_cse485;port=3306', 'root','');
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
@@ -15,6 +14,4 @@ class DBConnection{
     public function getConnection(){
         return $this->conn;
     }
-
-
 }
