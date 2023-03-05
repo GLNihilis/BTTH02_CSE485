42e313dc5b499c482778d26d9724591ba6031ecf
Nguyễn Đức Anh <99268985+GLNihilis@users.noreply.github.com>
2023-03-05 21:56:37 +0700

    update

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -6,24 +6,5 @@ include("services/LoginServices.php");
         {
             include("views/home/login.php");
         }
-
-        public function Authentication()
-        {
-            $username = $_POST['username'] ?? '';
-            $passowrd = $_POST['password'] ?? '';
-            if($username != '' and $passowrd != '')
-            {
-                $authentication = new LoginService();
-                if($authentication -> CheckLogin($username,$passowrd))
-                {
-                    header('Location: ?controller=admin');
-                }
-                else
-                {
-                    $message = "Invalid Data";
-                    include("view/home/login.php");
-                }
-            }
-        }
     }
 ?>
--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 // B1: Bắt giá trị controller và action
 $controller = isset($_GET['controller'])?   $_GET['controller']:'home';
 $action     = isset($_GET['action'])?       $_GET['action']:'index';
+$id         = isset($_GET['id'])?       $_GET['id'] : null;
 
 // B2: Chuẩn hóa tên trước khi gọi
 $controller = ucfirst($controller);
